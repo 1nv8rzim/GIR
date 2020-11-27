619ba39c91cb65b9d4044295f921f0f17fdf8623
Maxwell <msf9542@g.rit.edu>
2020-11-27 18:27:33 -0500

    skeleton

--- gir.py
+++ gir.py
@@ -8,9 +8,21 @@ gir = SlackClient(OAUTH_TOKEN)
 RTM_READ_DELAY = 1
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
+
+def parse_bot_commands(commands):
+    pass
+
+
+def handle_command(command, channel):
+    pass
+
+
 if __name__ == '__main__':
     if gir.rtm_connect(with_team_state=False):
         print("GIR has connected and is running!")
         starterbot_id = gir.api_call("auth.test")["user.id"]
         while True:
-            command, channel = parse_command(command)
+            command, channel = parse_bot_commands(gir.rtm_read())
+            if command:
+                handle_command(command, channel)
+            time.sleep(RTM_READ_DELAY)
