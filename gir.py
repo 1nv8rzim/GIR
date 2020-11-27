fcc6350a7fd737afb4b413b35450580b7cbebd6f
Maxwell <msf9542@g.rit.edu>
2020-11-27 18:52:41 -0500

    minor changes

--- gir.py
+++ gir.py
@@ -1,30 +1,31 @@
-from slackclient import SlackClient
+import slack
 from private.information import OAUTH_TOKEN
 import time
 import re
-
-gir = SlackClient(OAUTH_TOKEN)
 
 RTM_READ_DELAY = 1
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
 
-def parse_bot_commands(commands):
-    print(commands)
+class GIR:
+    def __init__(self, channel):
+        self.slack_bot = SlackClient(OAUTH_TOKEN)
+        self.channel = channel
 
+    def parse_bot_commands(commands):
+        print(commands)
 
-def handle_command(command, channel):
-    pass
+    def handle_command(command, channel):
+        pass
 
-
-if __name__ == '__main__':
-    if gir.rtm_connect(with_team_state=False):
-        print("GIR has connected and is running!")
-        starterbot_id = gir.api_call("auth.test")["user.id"]
-        while True:
-            command, channel = parse_bot_commands(gir.rtm_read())
-            if command:
-                handle_command(command, channel)
-            time.sleep(RTM_READ_DELAY)
-        else:
-            print("Connection failed")
+    if __name__ == '__main__':
+        if gir.rtm_connect(with_team_state=False):
+            print("GIR has connected and is running!")
+            starterbot_id = gir.api_call("auth.test")["user.id"]
+            while True:
+                command, channel = parse_bot_commands(gir.rtm_read())
+                if command:
+                    handle_command(command, channel)
+                time.sleep(RTM_READ_DELAY)
+            else:
+                print("Connection failed")
