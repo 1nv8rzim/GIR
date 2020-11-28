82c8babeacd82b850ecea1290c7781d6aafa777d
Maxwell <msf9542@g.rit.edu>
2020-11-27 19:53:29 -0500

    minor changes

--- gir.py
+++ gir.py
@@ -1,3 +1,18 @@
-from slack import WebClient
-from slack.errors import SlackApiErrors
-from private.information import OAUTH_TOKEN
+from slack_bolt import App
+from private.information import *
+
+app = App(
+    token=OAUTH_TOKEN,
+    signing_secret=SIGNING_SECRET
+)
+
+
+@app.command("/echo")
+def repeat_text(ack, say, command):
+    # Acknowledge command request
+    ack()
+    say(f"{command['text']}")
+
+
+if __name__ == '__main__':
+    app.start(port=3000)
