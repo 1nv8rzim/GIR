17cf80f633c4b9996efe12b7647d90aceb036da6
Maxwell <msf9542@g.rit.edu>
2020-11-28 19:28:59 -0500

    POF

--- gir.py
+++ gir.py
@@ -1,2 +1,7 @@
 from flask import Flask
-from slack import WebClient
+from private.information import OAUTH_TOKEN, SIGNING_SECRET
+import slack
+
+client = slack.WebClient(token=OAUTH_TOKEN)
+
+client.chat_postMessage(channel='#general', text='test')
