a6524030e7c75bb89153d5f40a1c44fab967d0a4
Maxwell <msf9542@g.rit.edu>
2020-11-29 15:04:35 -0500

    skeleton

--- gir.py
+++ gir.py
@@ -3,6 +3,16 @@ from flask import Flask, request, Response
 from private.information import OAUTH_TOKEN, SIGNING_SECRET
 from slackeventsapi import SlackEventAdapter
 
+# Intializing Flask Web App
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
+
+# Initializing Slack Web Client
 client = slack.WebClient(token=OAUTH_TOKEN)
 
-client.chat_postMessage(channel='#general', text='test')
+# Global Variables
+BOT_ID = client.api_call('auth.test')['user_id']
+
+# Run Web App
+if __name__ == '__main__':
+    app.run(debug=True)
