580c6ab88ab158ca251218212890bed46168276b
Maxwell <msf9542@g.rit.edu>
2020-11-28 19:17:47 -0500

    restart

--- gir.py
+++ gir.py
@@ -1,18 +1,2 @@
-from slack_bolt import App
-from private.information import *
-
-app = App(
-    token=OAUTH_TOKEN,
-    signing_secret=SIGNING_SECRET
-)
-
-
-@app.command("/echo")
-def repeat_text(ack, say, command):
-    # Acknowledge command request
-    ack()
-    say(f"{command['text']}")
-
-
-if __name__ == '__main__':
-    app.start(port=3000)
+from flask import Flask
+from slack import WebClient
